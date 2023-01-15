4866030ade50d457ee91f2ddb567019a9a7a36c4
ValeraVolkogonov <114488509+ValeraVolkogonov@users.noreply.github.com>
2023-01-15 21:45:12 +0300

    Теперь оно работает как должно)

--- src/view/sort-view.js
+++ src/view/sort-view.js
@@ -1,10 +1,11 @@
-import {createElement} from '../render.js';
+import { createElement } from '../render.js';
 
-function createListSortTemplate() {
+function createSortTemplate() {
   return (
-    `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
+    `
+    <form class="trip-events__trip-sort  trip-sort" action="#" method="get">
       <div class="trip-sort__item  trip-sort__item--day">
-        <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day">
+        <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day" checked>
         <label class="trip-sort__btn" for="sort-day">Day</label>
       </div>
       <div class="trip-sort__item  trip-sort__item--event">
@@ -16,20 +17,21 @@ function createListSortTemplate() {
         <label class="trip-sort__btn" for="sort-time">Time</label>
       </div>
       <div class="trip-sort__item  trip-sort__item--price">
-        <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price" checked>
+        <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price">
         <label class="trip-sort__btn" for="sort-price">Price</label>
       </div>
       <div class="trip-sort__item  trip-sort__item--offer">
         <input id="sort-offer" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-offer" disabled>
         <label class="trip-sort__btn" for="sort-offer">Offers</label>
       </div>
-    </form>`
+    </form>
+    `
   );
 }
 
-export default class ListSortView {
+export default class SortView {
   getTemplate() {
-    return createListSortTemplate();
+    return createSortTemplate();
   }
 
   getElement() {
