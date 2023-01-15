d7888c52c40ffa61734ba353f8115584f8f35960
ValeraVolkogonov <114488509+ValeraVolkogonov@users.noreply.github.com>
2023-01-15 21:57:14 +0300

    синхрон уд ветки и мастер

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import FilterView from './view/filter-view.js';
-import { render } from './render.js';
+import render from './render.js';
 import BoardPresenter from './presenter/board-presenter.js';
 
 const filterContainerElement = document.querySelector('.trip-controls__filters');
