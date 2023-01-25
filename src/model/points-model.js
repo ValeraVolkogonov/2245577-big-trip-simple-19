6dbf5ea1b9a40310460bbedae9338822a8a709a8
ValeraVolkogonov <114488509+ValeraVolkogonov@users.noreply.github.com>
2023-01-25 23:34:53 +0300

    Добавлены приват поля и геттеры

--- src/model/points-model.js
+++ src/model/points-model.js
@@ -1,32 +1,29 @@
 import { getPoints } from '../mock/point.js';
-// import { getRandomPoint } from '../mock/point.js';
 import { getOffers } from '../mock/offer.js';
 import { getOffersByType } from '../mock/offers-by-type.js';
 import { getDestinations } from '../mock/destination.js';
 
-//const POINT_COUNT = 3;
-
 export default class PointsModel {
 
-  // points = Array.from({ length: POINT_COUNT }, () => ({ ...getRandomPoint() }));
-  points = [...getPoints()];
-  offers = getOffers();
-  offersByType = getOffersByType();
-  destinations = getDestinations();
 
-  getPoints() {
+  #points = [...getPoints()];
+  #offers = getOffers();
+  #offersByType = getOffersByType();
+  #destinations = getDestinations();
+
+  get points() {
     return this.points;
   }
 
-  getOffers() {
+  get offers() {
     return this.offers;
   }
 
-  getOffersByType() {
+  get offersByType() {
     return this.offersByType;
   }
 
-  getDestinations() {
+  get destinations() {
     return this.destinations;
   }
 }
